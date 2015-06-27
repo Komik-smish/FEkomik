f8f38860dcd7564137793851fc8ffeca8ac128b3
Kelley Rose <krose422@gmail.com>
2015-06-27 13:50:07 -0400

    Added create page elements to home page so they show on slideup

--- app/js/komik/image.controller.js
+++ app/js/komik/image.controller.js
@@ -8,59 +8,15 @@
 
       function ($scope, $http, ImageService, HEROKU, $location) {
 
+        $scope.uploadImage = function(x) {
+          ImageService.upload(x).success( function() {
 
-        ImageService.getAssets().success( function (data) {
-          console.log(data.assets);
-          $scope.assetList = data.assets;
-        });
+            $('[data-id="'+ x.objectId + '"]').fadeOut( function () {
+              $scope.imageGroup = _.without($scope.imageGroup, x);
+            });
 
-        $('canvas').drawImage({
-          layer: true,
-          source: "http://i.imgur.com/0XYZC08.jpg",
-          width: 400,
-          height: 400,
-          x: 250, y: 250,
-          autosave: true,
-          crossOrigin: 'anonymous'
-        });
-
-        $scope.addToCanvas = function (assetUrl) {
-          console.log(assetUrl);
-          $('canvas').drawImage({
-            layer: true,
-            source: assetUrl,
-            draggable: true,
-            bringToFront: true,
-            width: 200,
-            height: 200,
-            x: 200, y: 200,
-            crossOrigin: 'anonymous',
           });
-          $(event.target).fadeOut();
         };
-
-        $scope.download = function () {
-          $('canvas').saveCanvas();
-          var image = $('canvas').getCanvasImage('png');
-          console.log(image);
-          var a = $("<a>").attr("href", image).attr("download", "img.png").appendTo("body");
-
-          a[0].click();
-          a.remove();
-        };
-
-
-
-    $scope.uploadImage = function(x) {
-      ImageService.upload(x).success( function() {
-
-
-          $('[data-id="'+ x.objectId + '"]').fadeOut( function () {
-            $scope.imageGroup = _.without($scope.imageGroup, x);
-          });
-
-        });
-      };
 
         $scope.uploadImage = function(x) {
           ImageService.upload(x).success( function() {
--- app/js/komik/imagelist.controller.js
+++ app/js/komik/imagelist.controller.js
@@ -14,12 +14,18 @@
         $scope.imageList = data;
       });
 
+      ImageService.getAssets().success( function (data) {
+        console.log(data.assets);
+        $scope.assetList = data.assets;
+      });
+
+
       $scope.store = function (image_url) {
 
         $('#all-pix').slideUp();
         $('#single-pix').slideDown();
 
-        $('#canvas').drawImage({
+        $('canvas').drawImage({
           layer: true,
           source: image_url,
           width: 400,
@@ -30,6 +36,30 @@
         });
       };
 
+      $scope.addToCanvas = function (assetUrl) {
+        console.log(assetUrl);
+        $('canvas').drawImage({
+          layer: true,
+          source: assetUrl,
+          draggable: true,
+          bringToFront: true,
+          width: 200,
+          height: 200,
+          x: 200, y: 200,
+          crossOrigin: 'anonymous',
+        });
+        $(event.target).fadeOut();
+      };
+
+      $scope.download = function () {
+        $('canvas').saveCanvas();
+        var image = $('canvas').getCanvasImage('png');
+        console.log(image);
+        var a = $("<a>").attr("href", image).attr("download", "img.png").appendTo("body");
+
+        a[0].click();
+        a.remove();
+      };
 
 
     }]);
