274c22273352cf9dd3657d4529877d8d47d5e842
Kelley Rose <krose422@gmail.com>
2015-06-26 21:44:10 -0400

    Set up controller files, installed angular-route

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,7 @@
       }
     }
   })
-  .config(['$routeProvider', function($routeProvider) {
+  .config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider.when('/', {
       controller: 'ImageList',
