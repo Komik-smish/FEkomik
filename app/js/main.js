e7130d5e28c3f79158e03e355050aef1d9c8ef1e
Joe Joyave <jjoyave1@gmail.com>
2015-06-26 20:36:51 -0400

    initial commit, adding base service file, base js files

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,29 @@
-console.log('Angular Rocks', angular);
+;(function (){
+
+  'use strict';
+
+  angular.module('Komik', ['ngRoute']).constant('PARSE', {
+    URL: 'https://www.parse.com/1/',
+    CONFIG: {
+
+      headers: {
+      'X-Parse-Application-Id': 'RiQBNnq9Zct2WTY7fCRA7EYr5euAisXXb0UWYy5E',
+      'X-Parse-REST-API-Key': 'AEGIes9PzVjnRLAE7sMxnoo8vGvU32joy8mmMCf2'
+      }
+    }
+  })
+  .config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.when('/', {
+      controller: 'ImageList',
+      templateUrl: "js/komik/home.tpl.html"
+    })
+
+    .when('/create', {
+      controller: 'Image',
+      templateUrl: 'js/komik/create.tpl.html'
+    });
+
+  }]);
+
+}());
