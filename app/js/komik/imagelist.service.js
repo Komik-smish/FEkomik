404a5fdcd1ee05c7ed10083e52d3f189efea89d2
Joe Joyave <jjoyave1@gmail.com>
2015-06-27 11:15:09 -0400

    Figured out canvas, removed text for later pulling from kelley, added functionality to image/imagelist

--- app/js/komik/imagelist.service.js
+++ app/js/komik/imagelist.service.js
@@ -14,7 +14,19 @@
       this.image_url = options.image_url;
     };
 
+    this.getAll = function() {
+      $http.get(endpoint, PARSE.CONFIG);
+    };
 
+    this.deleteImage = function(x) {
+      var URLtoDelete = endpoint + x.objectId;
+      return $http.delete(URLtoDelete, PARSE.CONFIG);
+    };
+
+    this.upload = function(x) {
+      var img = new Image(x);
+      return $http.post( endpoint, img, PARSE.CONFIG);
+    };
 
   }]);
 
